3ab42cc80b71abad65840699d66fc3fae8cee1b0
Andrii Kliuiko <akliuiko@Andriis-MacBook-Pro.local>
2024-12-18 10:35:50 +0200

    Fix tracing problems.

---
+++ order-service/src/main/java/com/kliuiko/configuration/OtlpConfiguration.java
@@ -0,0 +1,17 @@
+package com.kliuiko.configuration;
+
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OtlpConfiguration {
+
+    @Bean
+    OtlpHttpSpanExporter otlpHttpSpanExporter() {
+        return OtlpHttpSpanExporter.builder()
+                .setEndpoint("http://localhost:4318/v1/traces")
+                .build();
+    }
+
+}
